b8a96e69c99f986191e351679e9f60cd37d0b173
Debasish024 <69295275+Debasish024@users.noreply.github.com>
2020-10-23 22:13:51 +0530

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,11 @@
+print("welcome tobmy multiplication table")
+
+a = int(input("enter which number table you want :"))
+i =0
+
+while(True):
+      i+= 1
+      print(f"{a} x {i}",a*i) 
+      if i==10:
+            break
+      
